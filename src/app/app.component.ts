2752f05b2f71619f5b7321968ad84899cd7e1931
namratad127 <namratad127@gmail.com>
2024-06-20 14:48:08 +0530

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NewsReadingService } from './news-reading.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'task_management';
+  newsFeed : any = []
+  constructor(public httpGet:NewsReadingService) {}
+
+  ngOnInit() {
+    this.httpGet.getNewsList().subscribe(
+      (response :any) => {this.newsFeed = response.articles}
+    )
+  }
+
+  openArticle(url:string){
+    window.open(url, '_blank')
+  }
 }
+
+
