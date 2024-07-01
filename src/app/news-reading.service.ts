d60e46f2e2dea3fe8bfc4442e0051e7006cadc51
namratad127 <namratad127@gmail.com>
2024-07-02 02:02:37 +0530

    final commit

--- src/app/news-reading.service.ts
+++ src/app/news-reading.service.ts
@@ -9,7 +9,7 @@ export class NewsReadingService {
 
   constructor(public httpClient:HttpClient) { }
   
-  API_URL = "https://newsapi.org/v2/everything?q=tesla&from=2024-05-21&sortBy=publishedAt&apiKey=900feb0b813a4d00b5f04df1b10ea82d";
+  API_URL = "https://gnews.io/api/v4/search?q=example&apikey=26e0b7f76fc1f3801061dbc6e8da3138"
 
   getNewsList(): Observable<any[]> {
     return this.httpClient.get<any[]>(this.API_URL)
